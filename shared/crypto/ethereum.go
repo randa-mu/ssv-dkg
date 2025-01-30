a817fe47592b02a32805007a2c02a3b08a5d5371
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-01-30 13:07:27 +0100

    updated how deposit data message is created

--- shared/crypto/ethereum.go
+++ shared/crypto/ethereum.go
@@ -1,13 +1,14 @@
 package crypto
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/randa-mu/ssv-dkg/shared"
 )
 
 type RequiredDepositFields struct {
@@ -15,23 +16,29 @@ type RequiredDepositFields struct {
 	Amount                uint64
 }
 
-func PreDKGDepositDataMessage(data RequiredDepositFields) ([]byte, error) {
+func DepositDataMessage(data RequiredDepositFields, groupPublicKey []byte) ([]byte, error) {
 	if len(data.WithdrawalCredentials) != 32 {
 		return nil, errors.New("withdrawal credentials must be 32 bytes; actual length " + strconv.Itoa(len(data.WithdrawalCredentials)))
 	}
-	if data.Amount == 0 {
+
+	if data.Amount <= 0 {
 		return nil, errors.New("amount must be greater than zero")
 	}
 
-	return binary.BigEndian.AppendUint64(shared.Clone(data.WithdrawalCredentials), data.Amount), nil
-}
+	if len(groupPublicKey) != 48 {
+		return nil, errors.New("group public key must be 48 bytes long")
+	}
 
-func DepositDataMessage(data RequiredDepositFields, groupPublicKey []byte) ([]byte, error) {
-	msg, err := PreDKGDepositDataMessage(data)
+	b := bytes.Buffer{}
+	b.Write(groupPublicKey)
+	b.Write(data.WithdrawalCredentials)
+	err := binary.Write(&b, binary.BigEndian, data.Amount)
 	if err != nil {
 		return nil, err
 	}
-	return append(groupPublicKey, msg...), nil
+
+	hash := sha256.Sum256(b.Bytes())
+	return hash[:], nil
 }
 
 func ValidatorNonceMessage(address []byte, validatorNonce uint32) ([]byte, error) {
