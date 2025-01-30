1ee799c910e490868f28a59803ca4d842cb37955
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-01-30 12:07:13 +0100

    try reformatting object slightly

--- shared/api/deposit.go
+++ shared/api/deposit.go
@@ -28,9 +28,15 @@ type UnsignedDepositData struct {
 }
 
 type SignedDepositData struct {
-	UnsignedDepositData
-	PubKey    encoding.HexBytes `json:"pubkey"`
-	Signature encoding.HexBytes `json:"signature"`
+	WithdrawalCredentials encoding.HexBytes `json:"withdrawal_credentials"`
+	DepositDataRoot       encoding.HexBytes `json:"deposit_data_root"`
+	DepositMessageRoot    encoding.HexBytes `json:"deposit_message_root,omitempty"`
+	Amount                uint64            `json:"amount,omitempty"`
+	ForkVersion           string            `json:"fork_version,omitempty"`
+	NetworkName           string            `json:"network_name,omitempty"`
+	DepositCLIVersion     string            `json:"deposit_cli_version,omitempty"`
+	PubKey                encoding.HexBytes `json:"pubkey"`
+	Signature             encoding.HexBytes `json:"signature"`
 }
 
 type SigningOutput struct {
--- shared/files/deposit.go
+++ shared/files/deposit.go
@@ -8,11 +8,18 @@ import (
 )
 
 func CreateSignedDepositData(scheme crypto.ThresholdScheme, config api.SignatureConfig, output api.SigningOutput) api.SignedDepositData {
+	pk := crypto.ExtractGroupPublicKey(scheme, output.GroupPublicPolynomial)
 	var sig []byte
 	hex.Encode(sig, output.DepositDataSignature)
 	return api.SignedDepositData{
-		UnsignedDepositData: config.DepositData,
-		PubKey:              crypto.ExtractGroupPublicKey(scheme, output.GroupPublicPolynomial),
-		Signature:           sig,
+		WithdrawalCredentials: config.DepositData.WithdrawalCredentials,
+		DepositDataRoot:       config.DepositData.DepositDataRoot,
+		DepositMessageRoot:    config.DepositData.DepositMessageRoot,
+		Amount:                config.DepositData.Amount,
+		ForkVersion:           config.DepositData.ForkVersion,
+		NetworkName:           config.DepositData.NetworkName,
+		DepositCLIVersion:     config.DepositData.DepositCLIVersion,
+		PubKey:                pk,
+		Signature:             sig,
 	}
 }
