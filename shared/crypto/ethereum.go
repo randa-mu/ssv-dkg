37213193aff8dcb4e355a197dfa20583fe60a4f9
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-01-30 13:27:49 +0100

    additional domain hashing for deposit data message

--- shared/crypto/ethereum.go
+++ shared/crypto/ethereum.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"strconv"
@@ -16,7 +17,7 @@ type RequiredDepositFields struct {
 	Amount                uint64
 }
 
-func DepositDataMessage(data RequiredDepositFields, groupPublicKey []byte) ([]byte, error) {
+func DepositDataMessage(data RequiredDepositFields, forkVersion string, groupPublicKey []byte) ([]byte, error) {
 	if len(data.WithdrawalCredentials) != 32 {
 		return nil, errors.New("withdrawal credentials must be 32 bytes; actual length " + strconv.Itoa(len(data.WithdrawalCredentials)))
 	}
@@ -29,16 +30,31 @@ func DepositDataMessage(data RequiredDepositFields, groupPublicKey []byte) ([]by
 		return nil, errors.New("group public key must be 48 bytes long")
 	}
 
-	b := bytes.Buffer{}
-	b.Write(groupPublicKey)
-	b.Write(data.WithdrawalCredentials)
-	err := binary.Write(&b, binary.BigEndian, data.Amount)
+	treeRoot := bytes.Buffer{}
+	treeRoot.Write(groupPublicKey)
+	treeRoot.Write(data.WithdrawalCredentials)
+	err := binary.Write(&treeRoot, binary.BigEndian, data.Amount)
 	if err != nil {
 		return nil, err
 	}
 
-	hash := sha256.Sum256(b.Bytes())
-	return hash[:], nil
+	hashTreeRoot := sha256.Sum256(treeRoot.Bytes())
+
+	b := bytes.Buffer{}
+	b.Write(hashTreeRoot[:])
+
+	domain, err := computeDomain(forkVersion)
+	if err != nil {
+		return nil, err
+	}
+	b.Write(domain)
+	finalHash := sha256.Sum256(b.Bytes())
+	return finalHash[:], nil
+}
+
+func computeDomain(forkVersion string) ([]byte, error) {
+	domainDeposit := "03000000"
+	return hex.DecodeString(fmt.Sprintf("%s%s00000000", domainDeposit, forkVersion))
 }
 
 func ValidatorNonceMessage(address []byte, validatorNonce uint32) ([]byte, error) {
